198aaa233d2ca4b22039ef7c77213d9265009cd1
韩笑 <ylxs90@gmail.com>
2016-08-14 14:57:13 +0800

    load gradle build script, and add a FX file

---
+++ src/main/java/com/majiang/fx/Index.java
@@ -0,0 +1,20 @@
+/**
+ * Created by hxiao on 16/8/14.
+ */
+package com.majiang.fx;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Index extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
